08760cef4283df613104d0d453d88828c9bbb31b
Adam Hellberg <sharparam@sharparam.com>
2013-08-24 21:32:19 +0200

    Improved script description in PDHO script

--- PDHO_Flair_Linker.user.js
+++ PDHO_Flair_Linker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        PDHO Flair Linker
 // @namespace   http://sharparam.com/
-// @description Finds Steam flairs and makes them link to Steam profiles
+// @description Finds Steam flairs on /r/PaydayTheHeistOnline and makes them link to Steam profiles
 // @downloadURL https://github.com/Sharparam/UserScripts/raw/master/PDHO_Flair_Linker.user.js
 // @updateURL https://github.com/Sharparam/UserScripts/raw/master/PDHO_Flair_Linker.user.js
 // @include     http://www.reddit.com/r/paydaytheheistonline*
