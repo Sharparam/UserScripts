5f43b521aa72154d2da285d6c765a6c953bcc222
Adam Hellberg <sharparam@sharparam.com>
2013-08-26 01:07:36 +0200

    Should no longer match skill tree specification

--- PDHO_Flair_Linker.meta.js
+++ PDHO_Flair_Linker.meta.js
@@ -7,7 +7,7 @@
 // @include     http://www.reddit.com/r/paydaytheheistonline*
 // @include     http://reddit.com/r/paydaytheheistonline*
 // @include     https://pay.reddit.com/r/paydaytheheistonline*
-// @version     1.1.24
+// @version     1.1.25
 // @grant       GM_xmlhttpRequest
 // @run-at      document-end
 // ==/UserScript==
