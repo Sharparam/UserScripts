564b25e4a3a3902df930e4cb1967840e6209bd1d
Adam Hellberg <sharparam@sharparam.com>
2014-07-20 18:41:17 +0200

    Add Steam linkfilter bypass script

---
+++ Steam_linkfilter_bypass.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name Steam linkfilter bypass
+// @namespace steamlinkfilter
+// @description Bypasses the Steam linkfilter
+// @downloadURL https://github.com/Sharparam/UserScripts/raw/master/Steam_linkfilter_bypass.user.js
+// @updateURL https://github.com/Sharparam/UserScripts/raw/master/Steam_linkfilter_bypass.meta.js
+// @include https://steamcommunity.com/linkfilter/*
+// @version 1.0.0
+// @grant none
+// @run-at document-start
+// ==/UserScript==
