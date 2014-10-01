66858a768c2a370c8fcdf8329e9839a6756d1723
Adam Hellberg <sharparam@sharparam.com>
2014-10-02 00:20:43 +0200

    Add some more metadata

--- fb_comment_cleaner.meta.js
+++ fb_comment_cleaner.meta.js
@@ -2,6 +2,10 @@
 // @name Facebook comment cleaner
 // @namespace http://sharparam.com/
 // @description Cleans Facebook comments of idiotic name-only mentions
+// @copyright 2014, Sharparam (http://sharparam.com/)
+// @license MIT; http://opensource.org/licenses/MIT
+// @homepageURL https://github.com/Sharparam/UserScripts
+// @supportURL https://github.com/Sharparam/UserScripts/issues
 // @downloadURL https://github.com/Sharparam/UserScripts/raw/master/fb_comment_cleaner.user.js
 // @updateURL https://github.com/Sharparam/UserScripts/raw/master/fb_comment_cleaner.meta.js
 // @include /^https?://(www\.)?facebook.com(/.*)?$/
