6c9603b0309e5099277ed0034addc7ef6c3cbc96
Adam Hellberg <sharparam@sharparam.com>
2017-08-17 15:13:13 +0200

    Add steam link fixer script

---
+++ steam_link_fix/steam_link_fixer.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name        Steam link fixer
+// @namespace   https://sharparam.com
+// @description Fixes links on steam pages
+// @include     https://steamcommunity.com/*
+// @include     https://store.steampowered.com/*
+// @version     1.0.0
+// @grant       none
+// @license     MIT; http://opensource.org/licenses/MIT
+// @copyright   2017, Sharparam (http://sharparam.com/)
+// @homepageURL https://github.com/Sharparam/UserScripts
+// @supportURL  https://github.com/Sharparam/UserScripts/issues
+// @downloadURL https://github.com/Sharparam/UserScripts/raw/master/steam_link_fix/steam_link_fixer.user.js
+// @updateURL   https://github.com/Sharparam/UserScripts/raw/master/steam_link_fix/steam_link_fixer.meta.js
+// @run-at      document-end
+// ==/UserScript==
